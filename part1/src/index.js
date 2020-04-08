283b6aedfa3dc8fd6f2e322361e98f4ab5f7d266
lucasm08 <gompou16@univmetiers.ci>
2020-04-08 16:53:08 -0700

    part1 step3 exercice submission

--- part1/src/index.js
+++ part1/src/index.js
@@ -7,9 +7,9 @@ const Header = ({course}) => {
 }
 
 
-const Part = ({part, exercices}) => {
+const Part = ({part}) => {
   return (<p>
-    {part} {exercices}
+    {part.name} {part.exercices}
   </p>)
 }
 
@@ -19,12 +19,18 @@ const Total = ({total}) => {
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const part1 = {
+    name: 'Fundamentals of React',
+    exercises: 10
+  }
+  const part2 = {
+    name: 'Using props to pass data',
+    exercises: 7
+  }
+  const part3 = {
+    name: 'State of a component',
+    exercises: 14
+  }
 
   return (
     <div>
@@ -32,18 +38,16 @@ const App = () => {
 
       <Part
         part={part1}
-        exercises={exercises1}
       />
       <Part
         part={part2}
-        exercises={exercises2}
-      />
-      <Part
-        part={part3}
-        exercises={exercises3}
       />
 
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Part
+       part={part3} 
+      />
+
+      <Total total={part1.exercises + part2.exercises + part3.exercises}/>
      
     </div>
   )
