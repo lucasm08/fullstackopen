686f5fcc8daf98ea54da604bb27218bb3c39c75a
lucasm08 <gompou16@univmetiers.ci>
2020-04-12 06:59:04 -0700

    anecdotes step3

--- part1/annecdotes/src/index.js
+++ part1/annecdotes/src/index.js
@@ -1,26 +1,44 @@
 import React, { useState } from 'react'
 import ReactDOM from 'react-dom'
-import { isUserWhitespacable } from '@babel/types'
 
 const App = (props) => {
   const [selected, setSelected] = useState(0)
   const [votes, setVotes] = useState({0:0, 1:0, 2:0, 3:0, 4:0, 5:0})
+  const [popular, setPopular] = useState(0)
 
   const handleVotesUpdate = () => {
     const copy = {...votes}
     copy[selected] += 1
     setVotes(copy)
+    setPopular(getMostVotesIndex())
+  }
+
+  const getMostVotesIndex = () => {
+    let maxIndex = 0;
+    let maxVotes = 0;
+    for (const index in votes) {
+        if(votes[index] > maxVotes) {
+          maxVotes = votes[index];
+          maxIndex = index;
+        }
+    }
+    return maxIndex
+
   }
 
   return (
     <div>
+      <h1>Annecdote of the day</h1>
       {props.anecdotes[selected]}
       <p>has {votes[selected]} votes</p>
-  
       <div>
         <button onClick={handleVotesUpdate}>vote</button>
         <button onClick={() => setSelected(Math.floor((Math.random() * 6) + 1) - 1)}>next anecdote</button>
       </div>
+
+      <h1>Annecdote with most votes</h1>
+      {props.anecdotes[popular]}
+
     </div>
    
   )
