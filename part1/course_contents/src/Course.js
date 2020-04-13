0a77baff933f28b23aa4fb7f528cd2c6c92ab766
lucasm08 <gompou16@univmetiers.ci>
2020-04-12 21:09:08 -0700

    course contents step7

--- part1/course_contents/src/Course.js
+++ part1/course_contents/src/Course.js
@@ -3,10 +3,14 @@ import Header from './Header';
 import Content from './Content'
 
 const Course = ({course}) => {
+    const total = course.parts.reduce((s, p) => {
+        return s + p.exercises;
+    }, 0)
     return ( 
         <div>
             <Header name={course.name} />
             <Content parts={course.parts} />
+            <h4>total of {total} exercices</h4>
         </div>
      );
 }
