c700ae088da0101c5b1d88ce9831b0b7389303fb
lucasm08 <gompou16@univmetiers.ci>
2020-04-13 22:56:53 -0700

    the phonebook step3

--- part2/the_phonebook/src/App.js
+++ part2/the_phonebook/src/App.js
@@ -2,9 +2,13 @@ import React, { useState } from 'react'
 
 const App = () => {
   const [ persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { 
+      name: 'Arto Hellas',
+      number: '1-480-6780714'
+    }
   ]) 
-  const [ newName, setNewName ] = useState('a new name...')
+  const [ newName, setNewName ] = useState('')
+  const [ newNumber, setNewNumber] = useState('')
 
   const handleChange = (event) => {
     setNewName(event.target.value)
@@ -20,7 +24,8 @@ const App = () => {
       return;
     }
      const newNameObj =  {
-       name: newName
+       name: newName,
+       number: newNumber
      }
      setPersons(persons.concat(newNameObj))
      setNewName('')
@@ -35,13 +40,17 @@ const App = () => {
           name: <input onChange={handleChange} value={newName} />
         </div>
         <div>
+          number: <input onChange={(e) => { setNewNumber(e.target.value)}} value={newNumber} />
+        </div>
+
+        <div>
           <button type="submit">add</button>
         </div>
       </form>
       <h2>Numbers</h2>
       {
         persons.map(person => {
-          return (<p key={person.name}>{person.name}</p>)
+          return (<p key={person.name}>{person.name} {person.number}</p>)
         })
       }
     </div>
