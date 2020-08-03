3bdf046e3e5268a436ee976573b2d4fbedb878a8
lucasm08 <gompou16@univmetiers.ci>
2020-08-03 01:57:20 +0000

    Better anecdotes, step9

--- part6/unicafe-redux/src/components/AnecdoteList.js
+++ part6/unicafe-redux/src/components/AnecdoteList.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import { voteAnnecdote } from '../reducers/anecdoteReducer';
+import { notificationChange } from '../reducers/notificationReducer';
+import Notifcation from './Notifcation';
 import lodash from 'lodash';
 
 const Annecdote = ({ annecdote, handleClick }) => {
@@ -13,15 +15,26 @@ const Annecdote = ({ annecdote, handleClick }) => {
 };
 
 const AnnecdoteList = () => {
+  const handleVote = (anecdote) => {
+    dispatch(voteAnnecdote(anecdote.id));
+    const message = `you voted '${anecdote.content}'`;
+
+    dispatch(notificationChange(message));
+    setTimeout(() => {
+      dispatch(notificationChange(''));
+    }, 5000);
+  };
+
   const dispatch = useDispatch();
   const annecdotes = useSelector((state) => state.anecdotes);
   return (
     <div>
+      <Notifcation />
       {lodash.orderBy(annecdotes, ['votes'], ['desc']).map((annecdote) => (
         <Annecdote
           key={annecdote.id}
           annecdote={annecdote}
-          handleClick={() => dispatch(voteAnnecdote(annecdote.id))}
+          handleClick={() => handleVote(annecdote)}
         />
       ))}
     </div>
--- part6/unicafe-redux/src/components/Notifcation.js
+++ part6/unicafe-redux/src/components/Notifcation.js
@@ -3,10 +3,13 @@ import { useSelector } from 'react-redux';
 
 const Notification = () => {
   const notification = useSelector((state) => state.notification);
+  const display = notification === '' ? 'none' : '';
+
   const style = {
     border: 'solid',
     padding: 10,
     borderWidth: 1,
+    display: display,
   };
   return <div style={style}>{notification}</div>;
 };
